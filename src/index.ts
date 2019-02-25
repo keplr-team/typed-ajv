753a6fdbd4ebec985d2ca278c61e9da993a0bf14
Nicolas Even <nicolas.even@gokepler.fr>
2019-02-25 16:52:37 +0100

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,205 @@
+import * as _ from 'lodash';
+
+/**
+ * USAGE
+ *
+ * Define the common schema like this:
+ * const cs = CS.Object({
+ *   a: CS.String()
+ *   b: CS.Number()
+ * })
+ *
+ * Get the json schema :
+ * const schema = cs.getJsonSchema()
+ *
+ * Get the typescript type:
+ * type t = typeof cs.type // t = { a: string, b: number }
+ *
+ * HOW IT WORKS
+ *
+ * A CS function returns an object with the following properties:
+ * - getJsonSchema() => returns the part of the json schema corresponding to the CS
+ *   function (ex.: something like {"type": "string"} for CS.String)
+ * - T => a property of the typescript type corresponding to the CS function
+ *   (ex. : string for CS.String). The value doesn't matter.
+ *
+ * The magic occurs mainly in the function CS.Object, which combines the json
+ *  schemas and the typescript types of the properties given as arguments
+ */
+/*
+ * TODO :
+ *  - factorize the "required" logic for compound types
+ *  - make the "required" arguments optional
+ *  - add a "pick" function
+ */
+
+/** The return type of the CS.* functions */
+interface CommonSchema<T> {
+    type: T;
+    isRequired: boolean;
+    getJsonSchema: () => any;
+}
+
+/** The return type of the _* functions which are then transformed to CS.* functions with addRequiredArg */
+interface CommonSchemaWithoutIsRequired<T> {
+    type: T;
+    getJsonSchema: () => any;
+}
+
+function _String() {
+    return {
+        getJsonSchema: () => ({
+            type: 'string' as 'string',
+            transform: ['trim'],
+        }),
+        type: 'abcd' as string,
+    };
+}
+
+function _Number() {
+    return {
+        getJsonSchema: () => ({ type: 'number' as 'number' }),
+        type: 1234 as number,
+    };
+}
+
+function _Any() {
+    return {
+        getJsonSchema: () => ({}),
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        type: 'abcd' as any,
+    };
+}
+
+/** The two following types return a subtype that represent the required/optional keys of
+ * type T
+ */
+type GetRequiredKeys<T> = ({
+    [P in keyof T]: T[P] extends { isRequired: true } ? P : never
+})[keyof T];
+type GetOptionalKeys<T> = ({
+    [P in keyof T]: T[P] extends { isRequired: false } ? P : never
+})[keyof T];
+interface Props {
+    [key: string]: CommonSchema<any>;
+}
+
+function _Object<P extends Props, R extends boolean>(props: P, required: R) {
+    return {
+        getJsonSchema: () => {
+            const propsRequired = _.map(props, (v, k) =>
+                v.isRequired ? k : '',
+            ).filter(v => v !== '');
+
+            const ret = {
+                type: 'object',
+                properties: _.mapValues(props, v => v.getJsonSchema()),
+                additionalProperties: false,
+            };
+            return propsRequired.length > 0
+                ? { ...ret, required: propsRequired }
+                : ret;
+        },
+        type: (undefined as unknown) as {
+            [k in GetRequiredKeys<P>]: P[k]['type']
+        } &
+            { [k in GetOptionalKeys<P>]?: P[k]['type'] },
+        isRequired: (required as unknown) as R extends true ? true : false,
+    };
+}
+
+/* function _Optional<P extends CommonSchema<any>>(prop: P) {
+  return {
+    ..._.omit(prop, ['type', 'isRequired']),
+    type: (undefined as unknown) as P['type'] | undefined,
+    isRequired: false as false,
+  };
+} */
+
+function _Array<P extends CommonSchema<any>, R extends boolean>(
+    type: P,
+    required: R,
+) {
+    return {
+        getJsonSchema: () => {
+            return {
+                type: 'array',
+                items: type.getJsonSchema(),
+            };
+        },
+        type: (undefined as unknown) as P['type'][],
+        isRequired: (required as unknown) as R extends true ? true : false,
+    };
+}
+
+// TODO: more precise type to allow only objects as args
+function _MergeObjects<
+    A extends CommonSchema<any>,
+    B extends CommonSchema<any>,
+    R extends boolean
+>(obj1: A, obj2: B, required: R) {
+    return {
+        getJsonSchema: () => {
+            const s1 = obj1.getJsonSchema();
+            const s2 = obj2.getJsonSchema();
+            return {
+                type: 'object',
+                properties: { ...s1.properties, ...s2.properties },
+                required: [...(s1.required || []), ...(s2.required || [])],
+                additionalProperties: false,
+            };
+        },
+        type: (undefined as unknown) as A['type'] & B['type'],
+        isRequired: (required as unknown) as R extends true ? true : false,
+    };
+}
+
+/**
+ * string literals in TypeScript are of type string (not literal) by default so
+ * the arguments to Enum should be of the form ['abc' as 'abc',...] if you
+ * want the narrowest type possible
+ * @param els
+ */
+function _Enum<T extends string[], R extends boolean>(els: T, required: R) {
+    return {
+        getJsonSchema: () => {
+            return {
+                type: 'string',
+                enum: els,
+            };
+        },
+        type: (undefined as unknown) as T[0],
+        isRequired: (required as unknown) as R extends true ? true : false,
+    };
+}
+
+/** returns a CS function that takes the arguments of csFunc and a "required" argument as
+ * the last argument.
+ *
+ * We have a version of this function for each number of arguments of the original function
+ * because we can't use the spread operator if we want "required" as the last argument.
+ */
+function addRequiredArg0<Ret extends CommonSchemaWithoutIsRequired<any>>(
+    csFunc: () => Ret,
+) {
+    function withRequired<T extends boolean>(required: T) {
+        return {
+            ...csFunc(),
+            isRequired: (required as unknown) as T extends true ? true : false,
+        };
+    }
+    return withRequired;
+}
+
+export const CS = {
+    // Basic types
+    String: addRequiredArg0(_String),
+    Number: addRequiredArg0(_Number),
+    Any: addRequiredArg0(_Any),
+
+    // Compound types
+    Object: _Object,
+    Array: _Array,
+    MergeObjects: _MergeObjects,
+    Enum: _Enum,
+};
