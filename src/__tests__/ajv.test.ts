1ac9188f6d9e8fcd7c4fb2a15b38c88a02aaff70
Nicolas Even <nicolas.even@gokepler.fr>
2021-04-30 17:59:56 +0200

    Add another test for const

--- src/__tests__/ajv.test.ts
+++ src/__tests__/ajv.test.ts
@@ -36,7 +36,7 @@ describe('the schemas validate as expected with ajv', () => {
   });
 
   describe('Const', () => {
-    it('works properly', () => {
+    it('works in a basic case', () => {
       const schema = CS.Const('a' as const, true).getJsonSchema();
 
       const validate = new Ajv({ allErrors: true }).compile(schema);
@@ -45,6 +45,34 @@ describe('the schemas validate as expected with ajv', () => {
       expect(validate('a')).toBe(true);
       expect(validate('b')).toBe(false);
       expect(validate(1)).toBe(false);
+    });
+
+    it('works in an anyOf', () => {
+      const schema = CS.AnyOf(
+        [
+          CS.Object(
+            { type: CS.Const('car' as const, true), wheels: CS.Number(true) },
+            true,
+          ),
+          CS.Object(
+            { type: CS.Const('horse' as const, true), legs: CS.Number(true) },
+            true,
+          ),
+        ],
+        true,
+        { discriminator: 'type' },
+      ).getJsonSchema();
+
+      const validate = new Ajv({
+        allErrors: true,
+        removeAdditional: true,
+        discriminator: true,
+      }).compile(schema);
+
+      expect(validate({ type: 'horse', legs: 4 })).toBe(true);
+      expect(validate({ type: 'car', wheels: 2 })).toBe(true);
+
+      expect(validate({ type: 'horse', wheels: 2 })).toBe(false);
     });
   });
 });
